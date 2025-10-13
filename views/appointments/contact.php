cafce854848d7a0679b90d8e50ba6bb931fa18c2
volatianashalia <147123178+volatianashalia@users.noreply.github.com>
2025-10-13 17:47:28 +0300

    Update contact.php

--- views/appointments/contact.php
+++ views/appointments/contact.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../../assets/css/contact.css">
 </head>
 <body>
-        <?php require_once ('../layouts/header.php')?>
+        <?php require_once __DIR__'../layouts/header.php'?>
     <div class="container-fluid contact-hero" style="background-image: url('/ModeUnique/images/pexels-ron-lach-9849657.jpg');background-size: cover; 
     background-position: center; 
     background-repeat: no-repeat;
@@ -170,7 +170,7 @@
             </div>
         </div>
     </div>
-    <?php include_once ('../layouts/footer.php')?>
+    <?php require_once __DIR__ '../layouts/footer.php'?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <script src="../../assets/js/contact.js"></script>
 </body>
